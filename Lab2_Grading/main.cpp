0350bee6f52363aa62a92f24fe78fa0d10783390
JPN <jnicholls12@my.bcit.ca>
2021-01-09 11:31:52 -0800

    Starting lab2, copied over lab 1 files

---
+++ Lab2_Grading/main.cpp
@@ -0,0 +1,55 @@
+// Lab2_Grading.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "main.h"
+
+int number_of_student = 0;
+int main()
+{
+    struct student_Info new_Students[100];
+    uint8_t command; //var used to track the key press and decide what function to run
+
+    while (1)
+    {
+        print_Menu(); //Prints menu showing the commands
+        std::cout << "Choose commands: ";
+        std::cin >> command;//waits for input command
+
+        if (command == 'a' || command == 'A') //add student
+        {
+            add_Student(new_Students[number_of_student]);
+            number_of_student++;
+        }
+        else if (command == 'e' || command == 'E') // edit student
+        {
+            int temp_student_num;
+            std::cout << "Student to Edit: ";
+            std::cin >> temp_student_num;
+            if (temp_student_num >= number_of_student)//check to see if a valid student is selected
+            {
+                std::cout << "Student does not exist\n\n";
+            }
+            else
+            {
+                edit_Student(new_Students[temp_student_num]);
+            }
+        }
+        else if (command == 'p' || command == 'P') // Print Grades
+        {
+            for (int i = 0; i < number_of_student; i++)
+            {
+                print_Grades(new_Students[i], i);//print each of the students stored in the 'new_student' array
+            }
+            std::cout << "\n";
+        }
+        else if (command == 'q' || command == 'Q') // Quit Application
+        {
+            return 0;
+        }
+        else
+        {
+            std::cout << "Not a recognized command\n";
+        }
+    }
+}
---
+++ Lab2_Grading/main.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef MAIN_H
+#define MAIN_H
+
+#define CLASS_SIZE 100  // size of the class used to generate the struct of students
+
+#define LAB_COEFF       4
+#define FINAL_COEFF     3
+#define QUIZ_COEFF      1
+#define MIDTERM_COEFF   2
+#define WEIGHT_DIVISOR  10
+
+struct student_Info {
+    char    student_Num[9]; //
+    float   lab_Grade;
+    float   quiz_Grade;
+    float   midterm_Grade;
+    float   finalExam_Grade;
+};
+
+void add_Student(student_Info &new_Student);
+void edit_Student(student_Info &new_Student);
+void print_Grades(student_Info &new_Student, int current_student);
+void print_Menu();
+float final_grade(student_Info& new_Student);
+
+#endif // !MAIN_H
+
+

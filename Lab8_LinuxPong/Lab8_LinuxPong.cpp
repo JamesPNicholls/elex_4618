4f566f2e20d834bcb7bb327e2ad4ea8a18eb2aa3
JimLinux <JimLinux@gmail.com>
2021-03-11 22:37:50 -0800

    Added the linux port and CControlPi

---
+++ Lab8_LinuxPong/Lab8_LinuxPong.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+
+#include <iostream>
+#include <pigpio.h>
+#include <opencv2/opencv.hpp>
+
+#include "CBase4618.h"
+#include "CControl.h"
+#include "CPong.h"
+
+using namespace std;
+int main()
+{
+    int result = 0;
+    int cmd = 0;
+    bool passed = false;
+
+    if(gpioInitialise() < 0)
+    {
+        cout << "GPIO Init failed";
+    }
+    else
+    {
+        do
+        {
+            CControl demo;
+            demo.print_New_Menu();
+            cin >> cmd;
+
+            switch(cmd)
+            {
+            case 1: passed = demo.get_analog(analog, joyStick_X);       break;
+            case 2: passed = demo.get_data_poll(digital, push_Button1); break;
+            case 3: passed = demo.get_button(push_Button1);                         break;
+            case 4: passed = demo.set_servo();                          break;
+            case 5:
+                {
+                    CPong pong(cv::Size(PONG_CANVAS_WIDTH, PONG_CANVAS_HEIGHT), 0);
+                    pong.run();
+                    break;
+                }
+
+            }
+        }while(1);
+
+
+
+
+
+    }
+    gpioTerminate();
+}
+
